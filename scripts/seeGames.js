7abe9db043b3f6f119c1e04685ed9e22484d1f01
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2019-02-20 14:36:07 -0600

    adding script to check games in database.

---
+++ scripts/seeGames.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Game = require("../models/game");
+
+const hidePasswords = (users) => {
+  users.forEach(e => {
+    e.password="hidden!";
+    return e;
+  })
+  return users;
+}
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/shogiserver"
+);
+
+Game.find({})
+.then(dbGames => {
+  console.log(dbGames);
+});
