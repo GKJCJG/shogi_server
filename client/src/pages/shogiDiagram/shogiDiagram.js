30d96baf27d90b2b8f2f964b07eb3e1185cd26ec
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2019-03-16 12:48:50 -0500

    completed FENstring class definition.

--- client/src/pages/shogiDiagram/shogiDiagram.js
+++ client/src/pages/shogiDiagram/shogiDiagram.js
@@ -2,8 +2,26 @@ import React, {Component} from "react";
 import Board from "./../../components/board/diagramBoard";
 import gameLogic from "./../../utils/shogiBoard";
 
-class FenString {
+const symbolDictionary = {
+    p: "歩",
+    t:　"と",
+    l:　"香",
+    x: "杏",
+    n: "桂",
+    h: "圭",
+    s: "銀",
+    q: "全",
+    g: "金",
+    b: "角",
+    m: "馬",
+    r: "飛",
+    d: "竜",
+    k: ["玉", "王"]
+};
+
+class FenString extends String {
     constructor (string) {
+        super(string);
         this.FEN = string;
         this.FENArray = string.split("/");
     }
@@ -17,30 +35,56 @@ class FenString {
 
     translateRows() {
         let rows = {}
-        for (let i=0; i<9; i++) {
-            const singleRow = this.objectifyRow(this.FENArray[i]);
+        for (let i=9; i>0; i++) {
+            const singleRow = this.translateRow(i);
             Object.assign(rows, singleRow);
         }
         return rows;
     }
 
-    objectifyRow(rowString) {
-        let output = {};
+    translateRow(rowNumber) {
+        let output = {}, currentFile = 9, rowString = this.FENArray[rowNumber];
         for (let i = 0; i < rowString.length; i++) {
             if (isNaN(rowString[i])) {
                 handleSymbol(rowString[i]);
+                currentFile--;
             } else {
-                handleNumber(rowString[i])
+                handleNumber(parseInt(rowString[i]));
+                currentFile -= rowString[i];
             }
         }
+        return output;
         
         function handleSymbol(symbol) {
-            
+
+            let squareClass = /[A-Z]/.test(symbol) ? ["gote"] : ["sente"];
+            let occupant = symbolDictionary[symbol.toLowerCase()]
+
+            output["" + currentFile + rowNumber] = {class: squareClass, occupant};
+                        
         }
     
-        function handleNumber(number) {
-
+        function handleNumber(emptySquareCount) {
+            for (let i=0; i<emptySquareCount; i++) {
+                let currentSquare = "" + (currentFile-i) + rowNumber;
+                output[currentSquare] = {class: [null], occupant: null};
+            }
         }
+    }
+
+    translateHands() {
+        const senteHand = Object.assign(this.translateHand(this.fenArray[9]), {class: "sente"});
+        const goteHand = Object.assign(this.translateHand(this.fenArray[10]), {class: "gote"});
+        return {senteHand, goteHand};
+    }
+
+    translateHand(handString) {
+        let handOccupants = [];
+        const pieceOrder = ["歩", "香", "桂", "銀", "金", "角", "飛"];
+        for (let i = 0; i < pieceOrder.length; i++) {
+            handOccupants.push({symbol: pieceOrder[i], number: handString[i]});
+        }
+        return {occupants: handOccupants};
     }
 
     
