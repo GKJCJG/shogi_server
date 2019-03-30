1f63e21ad01b400d1715081d293721c3f2e76156
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2019-03-30 17:04:00 -0500

    More parsimonious syntax.

--- client/src/components/chat/chatBox.js
+++ client/src/components/chat/chatBox.js
@@ -1,11 +1,8 @@
 import React from "react";
 
 const ChatBox = (props) => {
-    let messages = [];
-    for (let i = props.messages.lengt - 1; i >= 0; i--) {
-        messages[props.messages.length - 1 - i] = props.messages[i];
-    }
-    return <div id="chatBox">{messages.map((e, i) => <p key={"message"+i}>{`#${e.moveNumber} ${e.author}: ${e.message}`}</p>)}</div>
+    let messages = props.messages.slice();
+    return <div id="chatBox">{messages.reverse().map((e, i) => <p key={"message"+i}>{`#${e.moveNumber} ${e.author}: ${e.message}`}</p>)}</div>
 };
 
 export default ChatBox;
