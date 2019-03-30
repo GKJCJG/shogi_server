008ae5044b8ee131f603dfadc9b0b37172a04d2d
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2019-03-30 16:46:37 -0500

    reverse safer than sort.

--- client/src/components/chat/chatBox.js
+++ client/src/components/chat/chatBox.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const ChatBox = (props) => <div id="chatBox">{props.messages.sort((a, b) => a.moveNumber - b.moveNumber).map((e, i) => <p key={"message"+i}>{`#${e.moveNumber} ${e.author}: ${e.message}`}</p>)}</div>;
+const ChatBox = (props) => <div id="chatBox">{props.messages.reverse().map((e, i) => <p key={"message"+i}>{`#${e.moveNumber} ${e.author}: ${e.message}`}</p>)}</div>;
 
 export default ChatBox;
