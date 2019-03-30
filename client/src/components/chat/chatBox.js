8059f963fd34f3f673a91522c8dd89412a7f7a1c
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2019-03-30 17:02:53 -0500

    not using reverse, as it mutates underlying array.

--- client/src/components/chat/chatBox.js
+++ client/src/components/chat/chatBox.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const ChatBox = (props) => <div id="chatBox">{props.messages.reverse().map((e, i) => <p key={"message"+i}>{`#${e.moveNumber} ${e.author}: ${e.message}`}</p>)}</div>;
+const ChatBox = (props) => {
+    let messages = [];
+    for (let i = props.messages.lengt - 1; i >= 0; i--) {
+        messages[props.messages.length - 1 - i] = props.messages[i];
+    }
+    return <div id="chatBox">{messages.map((e, i) => <p key={"message"+i}>{`#${e.moveNumber} ${e.author}: ${e.message}`}</p>)}</div>
+};
 
 export default ChatBox;
