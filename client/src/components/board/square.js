a3f68b7477315bb124c4686e8d2495da090338d9
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2019-03-30 22:24:05 -0500

    minor display adjustments

--- client/src/components/board/square.js
+++ client/src/components/board/square.js
@@ -9,13 +9,13 @@ const Square = (props) => {
     let classes = ["square"], occupantClass;
     classes = classes.concat(thisSquare.class);
     if (showPrevious && (horCoord + vertCoord) === position.lastMove) classes.push("previous");
-    if (["と", "杏", "圭", "全", "馬", "竜"].includes(thisSquare.occupant)) classes.push("promoted");
     if ((horCoord + vertCoord) === move.origin) classes.push("origin");
     if (classes.includes("gote")) occupantClass = "gote";
     className = classes.filter(e => e !== "gote").join(" ");
 
     if (thisSquare.occupant) {
         occupantClass = occupantClass || "sente";
+        if (["と", "杏", "圭", "全", "馬", "竜"].includes(thisSquare.occupant)) occupantClass += " promoted";
         occupant = (<div className = {"piece " + occupantClass}>{thisSquare.occupant}</div>);
     } else {
         occupant = null;
@@ -25,7 +25,8 @@ const Square = (props) => {
 
     if ((horCoord + vertCoord) === move.target) {
         const spText = move.piece ? nameDictionary[move.piece].symbol : position[move.origin].occupant;
-        const spClass = position[move.origin].class.includes("gote") ? "gote" : "sente";
+        let spClass = position[move.origin].class.includes("gote") ? "gote" : "sente";
+        if (["と", "杏", "圭", "全", "馬", "竜"].includes(spText)) spClass += " promoted";
         superPosition = (<div className = {"superPose " + spClass}>{spText}</div>);
     } else {
         superPosition = null;
